ca42e5ea1d6b962b7df016b9018b6e7325249271
Nhu-Hoai Robert Vo <nhuhoai.vo@nhuvo.ch>
2020-10-25 22:52:46 +0100

    Test GitHub Actions with PHPUnit.

---
+++ ci/test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo add-apt-repository ppa:ondrej/php
+sudo apt update
+sudo apt install -y wget mariadb-server postgresql "php${1}" "php${1}-mysql" "php${1}-pgsql"
+sudo service mysql start
+sudo service postgresql start
+sudo mysql < ./sql/mysql.sql
+sudo su -c "psql -f ./sql/postgre.sql" postgres
+sudo wget -O /usr/local/bin/phpunit https://phar.phpunit.de/phpunit-9.phar
+sudo chmod +x /usr/local/bin/phpunit
+php --version
+phpunit --version
+phpunit
