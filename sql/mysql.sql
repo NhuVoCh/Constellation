3ff2b411a314aeedf37010bc8b5d88037421ed6f
Nhu-Hoai Robert Vo <nhuhoai.vo@nhuvo.ch>
2020-10-25 22:12:22 +0100

    Create database for MySQL and PostgreSQL.

---
+++ sql/mysql.sql
@@ -0,0 +1,4 @@
+
+CREATE USER `constellation` IDENTIFIED BY 'constellation';
+CREATE DATABASE `constellation` CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+GRANT ALL PRIVILEGES ON `constellation`.* TO `constellation`;
